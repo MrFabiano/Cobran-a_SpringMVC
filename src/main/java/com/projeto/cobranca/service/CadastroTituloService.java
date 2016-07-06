d200ce6921a7f5309a0624916f5040233a782e09
Fabiano <fabiannosanttos@gmail.com>
2016-07-06 17:46:26 -0300

    cobrançaSpring

---
+++ src/main/java/com/projeto/cobranca/service/CadastroTituloService.java
@@ -0,0 +1,48 @@
+package com.projeto.cobranca.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Service;
+
+import com.projeto.cobranca.model.StatusTitulo;
+import com.projeto.cobranca.model.Titulo;
+import com.projeto.cobranca.repository.Titulos;
+import com.projeto.cobranca.repository.filter.TituloFilter;
+
+	@Service
+	public class CadastroTituloService {
+		
+		@Autowired
+		private Titulos titulos;
+		
+		public void salvar(Titulo titulo){
+			
+			try{
+			   titulos.save(titulo);
+		}catch(DataIntegrityViolationException e){
+			  throw new IllegalArgumentException("Formato de data inválida");
+		}
+	 }
+
+		public void excluir(Long codigo) {
+			titulos.delete(codigo);
+		}
+
+		public String receber(Long codigo) {
+			Titulo titulo = titulos.findOne(codigo);
+			titulo.setStatus(StatusTitulo.RECEBIDO);
+			titulos.save(titulo);
+			
+			return StatusTitulo.RECEBIDO.getDescricao();
+			
+		}
+		
+		public List<Titulo> filtrar(TituloFilter filtro){
+			String descricao = filtro.getDescricao() == null ? "%" : filtro.getDescricao();
+			return  titulos.findByDescricaoContaining(descricao);
+		}
+	}  
+
+
